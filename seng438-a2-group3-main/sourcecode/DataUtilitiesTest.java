b5062981bbc96b82965d23cfd3f0fa2b10e45cce
TsenguunU <73035276+TsenguunU@users.noreply.github.com>
2023-02-09 23:03:31 -0700

    Add files via upload
    finished getCumulativePercentage

--- seng438-a2-group3-main/sourcecode/DataUtilitiesTest.java
+++ seng438-a2-group3-main/sourcecode/DataUtilitiesTest.java
@@ -37,33 +37,6 @@ public class DataUtilitiesTest {
 	            will(returnValue(2));
 	        }
 	    });
-	    /*
-	    //expected output
-	    Mockery mockingContext1 = new Mockery();
-	    final KeyedValues expectedValues = mockingContext1.mock(KeyedValues.class);
-	    mockingContext1.checking(new Expectations() {
-	        {
-	        	//expected KeyedValues object {(0,0.4286),(1,1.0)}
-	            one(expectedValues).getIndex(0);
-	            will(returnValue(0));
-	            one(expectedValues).getIndex(1);
-	            will(returnValue(1));
-	            one(expectedValues).getKey(0);
-	            will(returnValue(0));
-	            one(expectedValues).getKey(1);
-	            will(returnValue(1));
-	            one(expectedValues).getKeys();
-	            List<Integer> keys = Arrays.asList(0,1);//list to store keys as integers
-	            will(returnValue(keys));
-	            one(expectedValues).getValue(0);
-	            will(returnValue(0.4286));
-	            one(expectedValues).getValue(1);
-	            will(returnValue(1.0));
-	            one(expectedValues).getItemCount();
-	            will(returnValue(2));
-	        }
-	    });
-	    */
 	    
 	    KeyedValues result = DataUtilities.getCumulativePercentages(values);
 	    
@@ -71,6 +44,95 @@ public class DataUtilitiesTest {
 	           0.4286, (Double)result.getValue(0), .000000001d);
 	    assertEquals("Returned value at key 1 should be 1.0",
 		           1.0, (Double)result.getValue(1), .000000001d);
+	}
+	    
+	    @Test
+		public void testNegPosCumulativePercentages() {
+			// setup
+		    Mockery mockingContext = new Mockery();
+		    final KeyedValues values = mockingContext.mock(KeyedValues.class);
+		    mockingContext.checking(new Expectations() {
+		        {
+		        	//KeyedValues object {(0,-9),(1,9.6), (2,2)}
+		            allowing(values).getIndex(0);
+		            will(returnValue(0));
+		            allowing(values).getIndex(1);
+		            will(returnValue(1));
+		            allowing(values).getIndex(2);
+		            will(returnValue(2));
+		            allowing(values).getKey(0);
+		            will(returnValue(0));
+		            allowing(values).getKey(1);
+		            will(returnValue(1));
+		            allowing(values).getKey(2);
+		            will(returnValue(2));
+		            allowing(values).getKeys();
+		            List<Integer> keys = Arrays.asList(0,1,2);//list to store keys as integers
+		            will(returnValue(keys));
+		            allowing(values).getValue(0);
+		            will(returnValue(-9));
+		            allowing(values).getValue(1);
+		            will(returnValue(9.6));
+		            allowing(values).getValue(2);
+		            will(returnValue(2));
+		            allowing(values).getItemCount();
+		            will(returnValue(3));
+		        }
+		    });
+		    
+		    KeyedValues result = DataUtilities.getCumulativePercentages(values);
+		    
+		    assertEquals("Returned value at key 0 should be -0.4368",
+		           -0.4368, (Double)result.getValue(0), .000000001d);
+		    assertEquals("Returned value at key 1 should be 0.23076923",
+			           0.23076923, (Double)result.getValue(1), .000000001d);
+		    assertEquals("Returned value at key 2 should be 1.0",
+			           1.0, (Double)result.getValue(2), .000000001d);
+	    
+	    // tear-down: NONE in this test method
+	}
+	    @Test
+		public void testOneZeroCumulativePercentages() {
+			// setup
+		    Mockery mockingContext = new Mockery();
+		    final KeyedValues values = mockingContext.mock(KeyedValues.class);
+		    mockingContext.checking(new Expectations() {
+		        {
+		        	//KeyedValues object {(0,7),(1,0), (2,2)}
+		            allowing(values).getIndex(0);
+		            will(returnValue(0));
+		            allowing(values).getIndex(1);
+		            will(returnValue(1));
+		            allowing(values).getIndex(2);
+		            will(returnValue(2));
+		            allowing(values).getKey(0);
+		            will(returnValue(0));
+		            allowing(values).getKey(1);
+		            will(returnValue(1));
+		            allowing(values).getKey(2);
+		            will(returnValue(2));
+		            allowing(values).getKeys();
+		            List<Integer> keys = Arrays.asList(0,1,2);//list to store keys as integers
+		            will(returnValue(keys));
+		            allowing(values).getValue(0);
+		            will(returnValue(7));
+		            allowing(values).getValue(1);
+		            will(returnValue(0));
+		            allowing(values).getValue(2);
+		            will(returnValue(2));
+		            allowing(values).getItemCount();
+		            will(returnValue(3));
+		        }
+		    });
+		    
+		    KeyedValues result = DataUtilities.getCumulativePercentages(values);
+		    
+		    assertEquals("Returned value at key 0 should be 0.777",
+		           0.777, (Double)result.getValue(0), .000000001d);
+		    assertEquals("Returned value at key 1 should be 0.777",
+			           0.777, (Double)result.getValue(1), .000000001d);
+		    assertEquals("Returned value at key 2 should be 1.0",
+			           1.0, (Double)result.getValue(2), .000000001d);
 	    
 	    // tear-down: NONE in this test method
 	}
