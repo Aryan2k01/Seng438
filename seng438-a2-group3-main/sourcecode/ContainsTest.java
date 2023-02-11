b9cb178f654d1c4d1a0fe8f959a7d67638c476d2
TsenguunU <73035276+TsenguunU@users.noreply.github.com>
2023-02-10 18:43:21 -0700

    Add files via upload
    reorganized the classes

--- seng438-a2-group3-main/sourcecode/ContainsTest.java
+++ seng438-a2-group3-main/sourcecode/ContainsTest.java
@@ -11,25 +11,27 @@ public class ContainsTest {
     @Before
     public void setUp() throws Exception { exampleRange = new Range(0.0, 100.0);
     }
+    //testing contains(double value) for P4: value=lower bound
 	@Test
 	public void testNumberLowerBound() {
 		Assert.assertTrue("Lower Bound not working", this.exampleRange.contains(0.0));
 	}
-	
+	//testing contains(double value) for P3: value=upper bound
 	@Test
 	public void testNumberUpperBound() {
 		Assert.assertTrue("Upper Bound not working", this.exampleRange.contains(100.0));
 	}
+	//testing contains(double value) for P1: value<lower bound
     @Test
 	public void testNumbersOutsideLowerBound() {
 		Assert.assertFalse("Number lower than Lower bound not working", this.exampleRange.contains(-1.00));
 	}
-	
+  //testing contains(double value) for P2: value>upper bound
 	@Test
 	public void testNumbersOutsideUpperBound() {
 		Assert.assertFalse("Number greater than upper bound not working", this.exampleRange.contains(101.00));
 	}
-	
+	//testing contains(double value) for P5: lower<value<upper
 	@Test
 	public void testNumbersInRange() {
 		Assert.assertTrue("Number in range not working", this.exampleRange.contains(78.0));
---
+++ seng438-a2-group3-main/sourcecode/CreateNumberArray2DTest.java
@@ -0,0 +1,51 @@
+package org.jfree.data.test;
+
+
+import static org.junit.Assert.*;
+
+import java.security.InvalidParameterException;
+
+import org.jfree.data.DataUtilities;
+
+import org.junit.Test;
+
+public class CreateNumberArray2DTest {
+	
+	private double [][] posdoubleArrayTest = {
+			 {1.0, 2.0, 3.0, 4.0},
+			 {5.0, 6.0, 7.0, 8.0},
+			 {9.0, 10.0, 11.0, 12.0}
+			 };
+	private double [][] negdoubleArrayTest = {
+			 {-1.0, -2.0, -3.0, -4.0},
+			 {-5.0, -6.0, -7.0, -8.0},
+			 {-9.0, -10.0, -11.0, -12.0}
+			 };
+	private Number [][] negnumberArrayTest = {
+			 {-1.0, -2.0, -3.0, -4.0},
+			 {-5.0, -6.0, -7.0, -8.0},
+			 {-9.0, -10.0, -11.0, -12.0}
+			 };
+	private Number [][] posnumberArrayTest = {{1.0, 2.0, 3.0, 4.0},{5.0, 6.0, 7.0, 8.0},{9.0, 10.0, 11.0, 12.0}};
+	private Number [][] numberArray, negNumberArray;
+
+	@Test
+	public void createPositiveNumberArray2D() {
+		numberArray = DataUtilities.createNumberArray2D(posdoubleArrayTest);
+		assertArrayEquals(posnumberArrayTest, numberArray);
+	}
+	@Test
+	public void createNegativeNumberArray2D() {
+		negNumberArray = DataUtilities.createNumberArray2D(negdoubleArrayTest);
+		assertArrayEquals(negnumberArrayTest, negNumberArray);
+	}
+
+	@Test (expected = InvalidParameterException.class)
+	public void createNumberArray2DInvalid() {
+		double [][] input = {};
+	    Number [][] result = DataUtilities.createNumberArray2D(input);
+			
+	}
+	
+
+}
